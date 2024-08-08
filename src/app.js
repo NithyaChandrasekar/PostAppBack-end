28d3ef3b214a82fd11f32a74c44e99fe2f0635be
NithyaChandrasekar <121688651+NithyaChandrasekar@users.noreply.github.com>
2024-08-08 18:29:54 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-    origin: "*",
+    origin: process.env.FRONTEND_URL,
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   }));
 
